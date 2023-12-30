8b99eddf6189c905196d73c0f9f8ab0fce924429
Rob Elsner <rob@elsner.dev>
2023-12-29 21:15:19 -0400

    Extract stats calcs and add a Pop

--- jitter_buffer.go
+++ jitter_buffer.go
@@ -1,21 +1,24 @@
 package jitterbuffer
 
 import (
-  "math"
-  "errors"
+	"errors"
 	"github.com/pion/rtp"
+	"math"
 )
 
 type JitterBufferState uint16
 
 type JitterBufferEvent string
+
 const (
 	Buffering JitterBufferState = iota
-	Emitting  
+	Emitting
 )
 const (
-  StartBuffering JitterBufferEvent = "startBuffering"
-  BeginPlayback = "playing"
+	StartBuffering JitterBufferEvent = "startBuffering"
+	BeginPlayback                    = "playing"
+  BufferUnderflow = "underflow"
+  BufferOverflow = "overflow"
 )
 
 func (jbs JitterBufferState) String() string {
@@ -25,7 +28,7 @@ func (jbs JitterBufferState) String() string {
 	case Emitting:
 		return "Emitting"
 	}
-  return "unknown"
+	return "unknown"
 }
 
 type JitterBufferOption struct {
@@ -37,20 +40,20 @@ type JitterBufferEventListener func(event JitterBufferEvent, jb *JitterBuffer)
 type JitterBuffer struct {
 	packets             [math.MaxUint16 + 1]*rtp.Packet
 	last_sequence       uint16
-  buffer_length       uint16
-  playout_head        uint16
+	buffer_length       uint16
+	playout_head        uint16
 	state               JitterBufferState
 	sample_rate         uint16
 	payload_sample_rate int
 	max_depth           int
 	stats               JitterBufferStats
-  listeners           []JitterBufferEventListener
+	listeners           []JitterBufferEventListener
 }
 
 type JitterBufferStats struct {
 	out_of_order_count uint32
 	empty_count        uint32
-	overflow_count     uint32
+	underflow_count    uint32
 	jitter             float32
 	max_jitter         float32
 }
@@ -64,41 +67,63 @@ func New(opts ...Option) *JitterBuffer {
 }
 
 func (jb *JitterBuffer) Listen(event JitterBufferEvent, cb JitterBufferEventListener) {
-  jb.listeners = append(jb.listeners, cb)
+	jb.listeners = append(jb.listeners, cb)
+}
+
+func (jb *JitterBuffer) updateStats(last_packet_seq_no uint16) {
+   // If we have at least one packet, and the next packet being pushed in is not
+  // at the expected sequence number increment the out of order count
+  if jb.buffer_length > 0 && last_packet_seq_no != ((jb.last_sequence + 1) % math.MaxUint16) {
+    jb.stats.out_of_order_count ++
+  }
+	jb.last_sequence = last_packet_seq_no
+	jb.buffer_length++
+	 
 }
 
 func (jb *JitterBuffer) Push(packet *rtp.Packet) {
 	jb.packets[packet.SequenceNumber] = packet
-	if packet.SequenceNumber != jb.last_sequence+1 &&
-		// we are wrapping around
-		(jb.last_sequence != math.MaxUint16 && packet.SequenceNumber == 0) {
-		jb.stats.overflow_count++
-	}
-  jb.last_sequence = packet.SequenceNumber
-  jb.buffer_length ++
+	
+  jb.updateStats(packet.SequenceNumber)
   jb.updateState()
 }
 
 func (jb *JitterBuffer) emit(event JitterBufferEvent) {
-  for _, l := range jb.listeners {
-    l(event, jb)
-  }
+	for _, l := range jb.listeners {
+		l(event, jb)
+	}
 }
 
 func (jb *JitterBuffer) updateState() {
-  // For now, we only look at the number of packets captured in the play buffer
-  if (jb.buffer_length >= 50) {
-    jb.state = Emitting
-    jb.emit(BeginPlayback)
-  }
+	// For now, we only look at the number of packets captured in the play buffer
+	if jb.buffer_length >= 50 {
+		jb.state = Emitting
+		jb.emit(BeginPlayback)
+	}
 }
 
 func (jb *JitterBuffer) Peek(playoutHead bool) (*rtp.Packet, error) {
-  if jb.buffer_length < 1 {
-    return nil, errors.New("Invalid Peek: Empty jitter buffer")
+	if jb.buffer_length < 1 {
+		return nil, errors.New("Invalid Peek: Empty jitter buffer")
+	}
+	if playoutHead {
+		return jb.packets[jb.playout_head], nil
+	}
+	return jb.packets[jb.last_sequence], nil
+}
+
+func (jb *JitterBuffer) Pop() (*rtp.Packet, error) {
+  if jb.state != Emitting {
+    return nil, errors.New("Attempt to pop while buffering")
   }
-  if playoutHead {
-    return jb.packets[jb.playout_head], nil
+  if jb.packets[jb.playout_head] == nil {
+    jb.stats.underflow_count ++
+    jb.emit(BufferUnderflow)
   }
-  return jb.packets[jb.last_sequence], nil
+  var packet = jb.packets[jb.playout_head]
+  jb.packets[jb.playout_head] = nil
+  jb.buffer_length --
+  jb.playout_head = (jb.playout_head +1 ) % math.MaxUint16
+  jb.updateState() 
+  return packet, nil
 }
--- jitter_buffer_test.go
+++ jitter_buffer_test.go
@@ -1,24 +1,24 @@
-package jitterbuffer 
+package jitterbuffer
 
 import (
-	"testing"
-  "github.com/stretchr/testify/assert"
 	"github.com/pion/rtp"
+	"github.com/stretchr/testify/assert"
+	"testing"
 )
 
 func TestJitterBuffer(t *testing.T) {
-  assert := assert.New(t)
-  t.Run("Appends packets in order", func (t *testing.T) {
-    jb := New()
-    assert.Equal(jb.last_sequence, uint16(0))
-	jb.Push(&rtp.Packet{Header: rtp.Header{SequenceNumber: 5000, Timestamp: 500}, Payload: []byte{0x02}})
-	jb.Push(&rtp.Packet{Header: rtp.Header{SequenceNumber: 5001, Timestamp: 501}, Payload: []byte{0x02}})
-	jb.Push(&rtp.Packet{Header: rtp.Header{SequenceNumber: 5002, Timestamp: 502}, Payload: []byte{0x02}})
+	assert := assert.New(t)
+	t.Run("Appends packets in order", func(t *testing.T) {
+		jb := New()
+		assert.Equal(jb.last_sequence, uint16(0))
+		jb.Push(&rtp.Packet{Header: rtp.Header{SequenceNumber: 5000, Timestamp: 500}, Payload: []byte{0x02}})
+		jb.Push(&rtp.Packet{Header: rtp.Header{SequenceNumber: 5001, Timestamp: 501}, Payload: []byte{0x02}})
+		jb.Push(&rtp.Packet{Header: rtp.Header{SequenceNumber: 5002, Timestamp: 502}, Payload: []byte{0x02}})
 
-    assert.Equal(jb.last_sequence, uint16(5002))
+		assert.Equal(jb.last_sequence, uint16(5002))
 
-	jb.Push(&rtp.Packet{Header: rtp.Header{SequenceNumber: 5012, Timestamp: 512}, Payload: []byte{0x02}})
+		jb.Push(&rtp.Packet{Header: rtp.Header{SequenceNumber: 5012, Timestamp: 512}, Payload: []byte{0x02}})
 
-    assert.Equal(jb.last_sequence, uint16(5012))
-  })
+		assert.Equal(jb.last_sequence, uint16(5012))
+	})
 }
