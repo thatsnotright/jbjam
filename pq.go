93227267ced97fefeb7f2cc5203d7886da2884a9
Rob Elsner <rob@elsner.dev>
2024-01-10 09:27:35 -0400

    Format

--- pq.go
+++ pq.go
@@ -8,30 +8,30 @@ import (
 )
 
 type PQ[T any] struct {
-  next *node[T]
-  length uint16
+	next   *node[T]
+	length uint16
 }
 
 type node[T any] struct {
-	val *T
-	next  *node[T]
-	prev  *node[T]
-	prio  uint16
+	val  *T
+	next *node[T]
+	prev *node[T]
+	prio uint16
 }
 
 func NewQueue[T any]() *PQ[T] {
 	return &PQ[T]{
 		next:   nil,
-    length: 0,
+		length: 0,
 	}
 }
 
 func newNode[T any](val *T, prio uint16) *node[T] {
 	return &node[T]{
-    val: val,
-		prev:   nil,
-		next:   nil,
-		prio:   prio,
+		val:  val,
+		prev: nil,
+		next: nil,
+		prio: prio,
 	}
 }
 func (q *PQ[T]) Find(sqNum uint16) (*T, error) {
@@ -40,32 +40,32 @@ func (q *PQ[T]) Find(sqNum uint16) (*T, error) {
 	}
 
 	if sqNum < q.next.prio {
-	  return nil, errors.New("No previous sequence")
+		return nil, errors.New("No previous sequence")
 	}
-  next:= q.next
-  for next!= nil {
-    if next.prio == sqNum {
-      return next.val, nil
-    }
-    next = next.next
-  }
-  return nil, errors.New("Priority item not found")
+	next := q.next
+	for next != nil {
+		if next.prio == sqNum {
+			return next.val, nil
+		}
+		next = next.next
+	}
+	return nil, errors.New("Priority item not found")
 }
 
 func (q *PQ[T]) Push(val *T, prio uint16) {
 	newPq := newNode(val, prio)
-  if q.next == nil {
-    q.next = newPq
-    q.length ++
-    return
-  }
-  if prio < q.next.prio {
-    newPq.next = q.next
-    q.next.prev = newPq
-    q.next = newPq
-    q.length ++
-    return
-  }
+	if q.next == nil {
+		q.next = newPq
+		q.length++
+		return
+	}
+	if prio < q.next.prio {
+		newPq.next = q.next
+		q.next.prev = newPq
+		q.next = newPq
+		q.length++
+		return
+	}
 	head := q.next
 	prev := q.next
 	for head != nil {
@@ -77,8 +77,8 @@ func (q *PQ[T]) Push(val *T, prio uint16) {
 	}
 	if head == nil {
 		if prev != nil {
-      prev.next = newPq
-    }
+			prev.next = newPq
+		}
 		newPq.prev = prev
 	} else {
 		newPq.next = head
@@ -88,7 +88,7 @@ func (q *PQ[T]) Push(val *T, prio uint16) {
 		}
 		head.prev = newPq
 	}
-  q.length ++
+	q.length++
 }
 
 func (q *PQ[T]) Length() uint16 {
@@ -96,11 +96,11 @@ func (q *PQ[T]) Length() uint16 {
 }
 
 func (q *PQ[T]) Pop() (*T, error) {
-  if q.next == nil {
-    return nil,  errors.New("Attempt to pop without a current value")
-  }
-  val := q.next.val
-  q.length --
-  q.next = q.next.next
-  return val, nil
+	if q.next == nil {
+		return nil, errors.New("Attempt to pop without a current value")
+	}
+	val := q.next.val
+	q.length--
+	q.next = q.next.next
+	return val, nil
 }
--- pq_test.go
+++ pq_test.go
@@ -1,7 +1,6 @@
 // SPDX-FileCopyrightText: 2023 The Pion community <https://pion.ly>
 // SPDX-License-Identifier: MIT
 
-
 package jitterbuffer
 
 import (
@@ -15,7 +14,7 @@ func TestPriorityQueue(t *testing.T) {
 	t.Run("Appends packets in order", func(t *testing.T) {
 		pkt := &rtp.Packet{Header: rtp.Header{SequenceNumber: 5000, Timestamp: 500}, Payload: []byte{0x02}}
 		q := NewQueue[rtp.Packet]()
-    q.Push(pkt, pkt.SequenceNumber)
+		q.Push(pkt, pkt.SequenceNumber)
 		pkt2 := &rtp.Packet{Header: rtp.Header{SequenceNumber: 5004, Timestamp: 500}, Payload: []byte{0x02}}
 		q.Push(pkt2, pkt2.SequenceNumber)
 		assert.Equal(q.next.next.val, pkt2)
@@ -33,9 +32,9 @@ func TestPriorityQueue(t *testing.T) {
 		for cur != nil {
 			last = cur
 			cur = cur.next
-      if cur != nil {
-      assert.Equal(cur.prio, last.prio + 1)
-      }
+			if cur != nil {
+				assert.Equal(cur.prio, last.prio+1)
+			}
 		}
 		assert.Equal(q.next.prio, uint16(5012))
 		assert.Equal(last.prio, uint16(5012+99))
@@ -43,17 +42,17 @@ func TestPriorityQueue(t *testing.T) {
 	t.Run("Can remove an element", func(t *testing.T) {
 		pkt := &rtp.Packet{Header: rtp.Header{SequenceNumber: 5000, Timestamp: 500}, Payload: []byte{0x02}}
 		q := NewQueue[rtp.Packet]()
-    q.Push(pkt, pkt.SequenceNumber)
+		q.Push(pkt, pkt.SequenceNumber)
 		pkt2 := &rtp.Packet{Header: rtp.Header{SequenceNumber: 5004, Timestamp: 500}, Payload: []byte{0x02}}
 		q.Push(pkt2, pkt2.SequenceNumber)
 		for i := 0; i < 100; i++ {
 			q.Push(&rtp.Packet{Header: rtp.Header{SequenceNumber: uint16(5012 + i), Timestamp: uint32(512 + i)}, Payload: []byte{0x02}}, uint16(5012+i))
 		}
-    popped, _:= q.Pop()
-    assert.Equal(popped.SequenceNumber, uint16(5000))
-    _, _ =  q.Pop()
-    nextPop, _ :=  q.Pop()
-    assert.Equal(nextPop.SequenceNumber, uint16(5012))
+		popped, _ := q.Pop()
+		assert.Equal(popped.SequenceNumber, uint16(5000))
+		_, _ = q.Pop()
+		nextPop, _ := q.Pop()
+		assert.Equal(nextPop.SequenceNumber, uint16(5012))
 	})
 	t.Run("Appends in order", func(t *testing.T) {
 		q := NewQueue[rtp.Packet]()
@@ -62,8 +61,8 @@ func TestPriorityQueue(t *testing.T) {
 		}
 		assert.Equal(uint16(100), q.Length())
 		pkt := &rtp.Packet{Header: rtp.Header{SequenceNumber: 5000, Timestamp: 500}, Payload: []byte{0x02}}
-    q.Push(pkt, pkt.SequenceNumber)
-    assert.Equal(pkt, q.next.val)
+		q.Push(pkt, pkt.SequenceNumber)
+		assert.Equal(pkt, q.next.val)
 		assert.Equal(uint16(101), q.Length())
 		assert.Equal(q.next.prio, uint16(5000))
 	})
